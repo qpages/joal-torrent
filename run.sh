19fcde37e5004d6d8f3dfb15d751faa619f7015b
quentin pagès <qpages@protonmail.ch>
2023-05-01 18:07:39 +0200

    change source file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source .env
+source .env.local
 
 if [[ $* == *--ui* ]]; then
   java -jar ./jack-of-all-trades-2.1.33.jar --joal-conf=$JOAL_CONF --spring.main.web-environment=true --server.port=833 --joal.ui.path.prefix=$JOAL_UI_PREFIX --joal.ui.secret-token=$JOAL_UI_SECRET_TOKEN
